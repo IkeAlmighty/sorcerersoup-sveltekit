8577002a383fdf554cdf678e7c70a00575c90934
IkeAlmighty <isaacyates7@gmail.com>
2023-06-15 23:27:14 -0500

    started the character sheet page

---
+++ src/lib/components/Item.svelte
@@ -0,0 +1,62 @@
+<script>
+
+    /**
+	 * @type {{ name: string; description: string; tier: number}}
+	 */
+     export let json;
+
+     let detailsViewable = false;
+</script>
+
+<div id="container">
+    <div id="item-header">
+        <b>{json.name}</b>
+        <hr class="inline-rule"/>
+        <span class="details-button" on:click={()=>{ detailsViewable = !detailsViewable }}>{detailsViewable? "hide": "view"}</span>
+    </div>
+
+    {#if detailsViewable}
+        <div class="item-details-container">
+            <i>{json.description}</i>
+        </div>
+    {/if}
+    
+</div>
+
+<style>
+
+    #container {
+        margin: 1rem 0;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-radius: 5px;
+    }
+
+    i {
+        display: block;
+        margin: 0.5rem 0;
+    }
+
+    .item-details-container {
+        margin-top: 2rem;
+    }
+
+    .details-button {
+        border: none;
+        cursor: pointer;
+        text-align: right;
+        user-select: none;
+    }
+
+    .inline-rule {
+        display: inline-block;
+        width: 100%;
+        max-height: 1px;
+        margin-left: 1rem;
+    }
+
+    #item-header {
+        display: grid;
+        grid-template-columns: auto 1fr 75px;
+    }
+</style>
---
+++ src/routes/character/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+
+    import { onMount } from 'svelte'
+    import Item from '$lib/components/Item.svelte';
+	import UnlockedIcon from '$lib/components/UnlockedIcon.svelte';
+	import LockedIcon from '$lib/components/LockedIcon.svelte';
+
+    let lockState = "unlocked"
+    function toggleLockState() {
+        if (lockState === "unlocked") lockState = "locked";
+        else lockState = "unlocked";
+    }
+
+    /**
+	 * @type {any[]}
+	 */
+    let itemsJSON = [];
+
+    onMount(()=>{
+        // fetch itemdata
+        // TODO: replace test data with real fetch to database:
+        itemsJSON = [
+            {name: "Amulet of Radiant Blessing", tier: 2, description: "A shimmering amulet adorned with a radiant gemstone. When worn, it emits a soft glow that grants the wearer enhanced healing abilities, allowing them to mend wounds more rapidly.", spells: []},
+            {name: "Blade of Frostbite", tier: 1, description: "A gleaming silver sword with an icy aura. The blade carries a chilling enchantment that freezes enemies upon contact, leaving them vulnerable to subsequent attacks and slowing their movements.", spells: []},
+            {name: "Potion of Elixir Mastery", tier: 3, description: "A small vial containing a potent elixir. When consumed, it temporarily enhances the drinker's alchemical abilities, granting them superior potion-making skills and increased effectiveness in brewing magical concoctions.", spells: []},
+            {name: "Gauntlets of Thunderclap", tier: 5, description: "Heavy gauntlets crackling with electrical energy. When activated, the gauntlets allow the wearer to unleash powerful lightning strikes with a mere gesture, striking foes with bolts of electrifying force.", spells: []},
+            {name: "Cloak of Shadows", tier: 2, description: "A dark, billowing cloak that seems to absorb light. When worn, it shrouds the wearer in an impenetrable veil of shadows, rendering them virtually invisible to the naked eye and granting enhanced stealth", spells: []},
+            {name: "Orb of Astral Projection", tier: 5, description: "A mystical orb pulsating with ethereal energy. When held, it allows the user to project their consciousness beyond the physical realm, exploring distant planes of existence and interacting with spirits and astral entities.", spells: []},
+        ]
+    });
+
+</script>
+
+<input id="character-name" type="text" value="Character Name" />
+<button id="lock-state-button" on:click={toggleLockState}>
+{#if lockState === "unlocked"} <UnlockedIcon /> {:else} <LockedIcon /> {/if}
+</button>
+<hr />
+
+<div>
+    <h3>Items</h3>
+    {#each itemsJSON as itemJSON}
+        <Item json={itemJSON} />
+    {/each}
+
+    <button>+ Add Item</button>
+</div>
+
+
+<style>
+    #lock-state-button {
+        width: 3rem;
+        float: right;
+    }
+
+    #character-name {
+        font-size: 1.5rem;
+        padding: 0.61rem;
+    }
+</style>
