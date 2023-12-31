9b51b0e83ca097a827da2afef66c0dceebdeebd7
IkeAlmighty <isaacyates7@gmail.com>
2023-12-30 18:00:15 -0600

    feat: started work on scene manager

---
+++ src/lib/components/SceneManager.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Scene from './Scene.svelte';
+	import { onMount } from 'svelte';
+
+	let scenes = [];
+
+	onMount(() => {
+		fetchAndSetScenes();
+	});
+
+	async function fetchAndSetScenes() {}
+</script>
+
+{#each scenes as scene}
+	<Scene items={scene.items} />
+{/each}
