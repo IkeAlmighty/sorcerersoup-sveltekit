a4d1343aebab855e714ba8610fa870ebccdbf497
IkeAlmighty <isaacyates7@gmail.com>
2023-12-30 18:01:14 -0600

    feat: layout page added with styles

---
+++ src/lib/components/TierRadioInput.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let onSelect = () => {};
+</script>
+
+<h3>Select a Tier</h3>
+<input type="radio" value="1" name="tier" on:click={() => onSelect('1')} />
+<label for="1">1</label>
+<input type="radio" value="2" name="tier" on:click={() => onSelect('2')} />
+<label for="2">2</label>
+<input type="radio" value="3" name="tier" on:click={() => onSelect('3')} />
+<label for="3">3</label>
+<input type="radio" value="4" name="tier" on:click={() => onSelect('4')} />
+<label for="4">4</label>
+<input type="radio" checked value="5" name="tier" on:click={() => onSelect('5')} />
+<label for="5">5</label>
+
+<style>
+	input[type='radio'] {
+		margin-right: 5px;
+		width: 1.5em;
+		height: 1.5em;
+	}
+
+	label {
+		margin-right: 15px;
+		font-size: 1.5em;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,34 @@
     <slot></slot>
 </div>
 
+<div id="footer">
+    <div>A creation of Ike Yates &lt;3</div>
+</div>
+
 <style>
 
     #container {
         margin: auto auto;
+        padding: 1rem;
         max-width: 800px;
+        min-height: 100vh;
+        border-radius: 0.2rem;
+        background-color: var(--secondary-color);
+        /* transform: translateY(20%) */
+    }
+
+    #footer {
+        position: fixed;
+        bottom: 0px;
+        left: 0;
+        width: 100vw;
+        height: 20px;
+        background-color: black;
+        color: white;
+        text-align: right;
+        padding: 0.2rem;
+    }
+    #footer * {
+        margin-right: 5rem;
     }
 </style>
